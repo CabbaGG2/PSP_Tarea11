53b51b6f71a6a0afbfbe0e9cb3b7170d7da416f9
CabbaGG2 <jcamaradepablos@danielcastelao.org>
2025-10-02 14:30:23 +0200

    Se arregló la lógica de hilos aleatorios, el hilo principal cuenta el tiempo total del programa y se creo una interfaz por terminal con validaciones sencillas que si no se cumplen se usan valores por defecto.

--- src/Hilo.java
+++ src/Hilo.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 // src/Hilo.java
 public class Hilo extends Thread {
-    private final int numeroIteraciones = 5;
+    private static int numeroHilos = 5;
     private int idHilo;
     private Hilo siguienteHilo;
 
@@ -11,21 +13,21 @@ public class Hilo extends Thread {
 
     @Override
     public void run() {
+        // Esto crea el siguiente hilo y lo inicializa
+        if (idHilo < numeroHilos) {
+            siguienteHilo = new Hilo(idHilo + 1);
+            siguienteHilo.start();
+        }
 
-        // Iteraciones del hilo actual
-        for (int i = 0; i < numeroIteraciones; i++) {
+        // Aqui itera el hilo que esta corriendo
+        for (int i = 0; i < 5; i++) {
             System.out.println("Soy el hilo: " + getName() + " y voy por la iteración: " + (i + 1));
             try {
-                Thread.sleep(1000);
+                int segundosAleatorio = (int) (Math.random() * 6) + 1; // Genera un número aleatorio entre 1 y 6
+                Thread.sleep(segundosAleatorio * 1000); // Duerme el hilo por ese número de segundos
             } catch (InterruptedException e) {
                 System.out.println("Ocurrió un error: " + e.getMessage());
             }
-        }
-
-        // Crear el siguiente hilo si corresponde
-        if (idHilo < 5) {
-            siguienteHilo = new Hilo(idHilo + 1);
-            siguienteHilo.start();
         }
 
         // Esperar a que termine el hijo (si existe)
@@ -42,13 +44,32 @@ public class Hilo extends Thread {
 
     public static void main(String[] args) {
         System.out.println("Programa inicializado");
-        Hilo hilo1 = new Hilo(1);
-        hilo1.start();
-        try {
-            hilo1.join(); // Esperar a que todos los hilos acaben
-        } catch (InterruptedException e) {
-            System.out.println("Error al esperar hilo 1: " + e.getMessage());
-        }
+        do {
+            long Inicio = System.currentTimeMillis();
+            Scanner sc  = new Scanner(System.in);
+            System.out.println("Escribe el numero de hilos que quieres que iteren o escribe 'exit' para salir:");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Terminando el programa...");
+                break;
+            }
+            if (input.isEmpty() || !input.matches("\\d+") || Integer.parseInt(input) < 1) {
+                System.out.println("Valor invalido, procedo a utilizar valores por defecto");
+                input = "5";
+            }
+            numeroHilos = Integer.parseInt(input);
+
+            Hilo hilo1 = new Hilo(1);
+            hilo1.start();
+            try {
+                hilo1.join(); // Esperar a que todos los hilos acaben
+            } catch (InterruptedException e) {
+                System.out.println("Error al esperar hilo 1: " + e.getMessage());
+            }
+            long fin = System.currentTimeMillis();
+            System.out.println("Tiempo total de la caída: " + (fin - Inicio) + " ms");
+        } while (true);
+
         System.out.println("Programa terminado");
     }
 }
